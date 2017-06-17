7b51f11a37d44da1a1c2656fc0e9eb783efecdf0
Stefan <Stefan@STEFAN-PC>
2017-06-17 12:49:36 +0200

    Removed commented out code

--- MetaExplorer/MetaExplorerBE/Cache/BaseThumbnailCache.cs
+++ MetaExplorer/MetaExplorerBE/Cache/BaseThumbnailCache.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 
 namespace MetaExplorerBE
@@ -22,48 +19,6 @@ namespace MetaExplorerBE
         }
 
         #endregion
-
-        //public override Task InitCacheAsync(IProgress<int> progress, IProgress<string> progressFile)
-        //{
-        //    return Task.Factory.StartNew(() =>
-        //    {
-        //        progress.Report(0);
-        //        progressFile.Report("");
-
-        //        //check if directory exists
-        //        if (!Directory.Exists(myThumbnailPath))
-        //        {
-        //            string errorMsg = String.Format("Directory does not exist: <{0}>.", myThumbnailPath);
-        //            Console.WriteLine(errorMsg);
-        //        }
-
-        //        //update progress
-        //        int idx = 0;
-        //        progressFile.Report(myThumbnailPath);
-
-        //        //read all files and throw away all files which do not have a proper extension
-        //        var validThumbnails = Directory.GetFiles(myThumbnailPath).ToList();
-        //        //validThumbnails.RemoveAll(x =>
-        //        //{
-        //        //    string extension = Path.GetExtension(x);
-        //        //    return !this.mySupportedImageFormats.Contains(extension, StringComparer.CurrentCultureIgnoreCase);
-        //        //});
-
-        //        //create thumbnails
-        //        foreach (string thumbnail in validThumbnails)
-        //        {
-        //            BitmapSource bs = this.CreateReducedThumbnailImage(new Uri(thumbnail), this.myThumbnailHeight, this.myThumbnailWidth);
-        //            bs.Freeze();
-        //            this.CachedItems.Add(new FileInfo(thumbnail), bs);
-
-        //            progress.Report((idx * 100) / validThumbnails.Count);
-        //        }
-        //        //}
-
-        //        progress.Report(100);
-        //        progressFile.Report("");
-        //    });
-        //}
 
         public BitmapSource GetByFilename(string filename)
         {
