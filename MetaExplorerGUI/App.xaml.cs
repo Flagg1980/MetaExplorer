2326e994d90b0a16af9bf8e73bfee3ddbd3d750b
Stefan <Stefan@STEFAN-PC>
2020-02-26 18:34:38 +0100

    App configurations

--- MetaExplorerGUI/App.xaml.cs
+++ MetaExplorerGUI/App.xaml.cs
@@ -15,39 +15,17 @@ namespace MetaExplorerGUI
     /// </summary>
     public partial class App : Application
     {
-        //private IHost _host;
         private IConfigurationRoot configRoot;
 
         public App() : base()
         {
             this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
-            //var bla = Directory.GetCurrentDirectory();
-            //_host = new HostBuilder()
-            //    .ConfigureHostConfiguration(config =>
-            //        config.SetBasePath(Directory.GetCurrentDirectory())
-            //              .AddJsonFile("appsettings.json", optional: false))
-            //    .ConfigureAppConfiguration(config =>
-            //        config.SetBasePath(Directory.GetCurrentDirectory())
-            //              .AddJsonFile("appsettings.json", optional: false))
-            //    .ConfigureServices((context, services) =>
-            //    {
-            //        //services.AddOptions();
-            //        services.Configure<Settings>(context.Configuration.GetSection("section0"));
-            //        services.AddSingleton<ITextService, TextService>();
-            //        services.Configure<Application>(context.Configuration.GetSection("section0"));
-
-            //        //                    services.AddSingleton<MainWindow>();
-            //    })
-            //    .Build();
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables();
-            configRoot = builder.Build();
-
-            //_host.Start();
-            
+            configRoot = builder.Build();         
         }
 
         void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
@@ -61,15 +39,11 @@ namespace MetaExplorerGUI
         [STAThread]
         private void ApplicationStart(object sender, StartupEventArgs e)
         {
-           // _host.Start();
-            //var config = _host.Services.GetService<ITextService>();
             var config = configRoot.GetSection("section0");
             CriteriaConfig.LoadFromAppSettings(config.GetValue<string>("CriterionConfig"));
 
-            //show the main window
             var mainWindow = new MainWindow(config);
             mainWindow.Show();
-
             mainWindow.DoInitStuff();
 
             foreach (Criterion crit in CriteriaConfig.Criteria)
